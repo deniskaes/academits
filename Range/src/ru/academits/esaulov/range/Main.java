07dea64df13b0051151e54c4befb5d745485ac18
Denis <deniskaes@gmail.com>
2018-05-31 03:54:37 +0700

    для проверки уже годится

--- Range/src/ru/academits/esaulov/range/Main.java
+++ Range/src/ru/academits/esaulov/range/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Range range1 = new Range(1, 5);
         Range range2 = new Range(3, 6);
+        Range range3 = new Range(7, 13);
 
         System.out.println(range1.getRangeLength());
         System.out.println(range1.isInside(3));
         System.out.println(range1.isInside(6));
-        System.out.println();
+        System.out.println(range2.crossingRanges(range1));
+
+        for (Range e : range1.concatenationRanges(range3)) {
+            System.out.println(e);
+        }
+
+        for (Range e : range1.differenceRanges(range2)) {
+            System.out.println(e);
+        }
     }
 }
--- Range/src/ru/academits/esaulov/range/Range.java
+++ Range/src/ru/academits/esaulov/range/Range.java
@@ -33,57 +33,57 @@ public class Range {
         return (testNumber >= from && testNumber <= to);
     }
 
-    public Range[] concatenationRanges(Range range1, Range range2) {
+    public Range[] concatenationRanges(Range range2) {
         Range[] arrayRanges = new Range[2];
 
-        if (range1.isInside(range2.from) && range1.isInside(range2.to)) {
-            arrayRanges[0] = range1;
+        if (this.isInside(range2.from) && this.isInside(range2.to)) {
+            arrayRanges[0] = this;
             arrayRanges[1] = null;
-        } else if (range2.isInside(range1.from) && range2.isInside(range1.to)) {
+        } else if (range2.isInside(this.from) && range2.isInside(this.to)) {
             arrayRanges[0] = range2;
             arrayRanges[1] = null;
-        } else if (range1.isInside(range2.from) && range2.isInside(range1.to)) {
-            arrayRanges[0] = new Range(range1.from, range2.to);
+        } else if (this.isInside(range2.from) && range2.isInside(this.to)) {
+            arrayRanges[0] = new Range(this.from, range2.to);
             arrayRanges[1] = null;
-        } else if (range2.isInside(range1.from) && range1.isInside(range2.to)) {
-            arrayRanges[0] = new Range(range2.from, range1.to);
+        } else if (range2.isInside(this.from) && this.isInside(range2.to)) {
+            arrayRanges[0] = new Range(range2.from, this.to);
             arrayRanges[1] = null;
         } else {
-            arrayRanges[0] = range1;
+            arrayRanges[0] = this;
             arrayRanges[1] = range2;
         }
         return arrayRanges;
     }
 
-    public Range[] differenceRanges(Range range1, Range range2) {
+    public Range[] differenceRanges(Range range2) {
         Range[] arrayRanges = new Range[2];
-        Range crossRange = crossingRanges(range1, range2);
+        Range crossRange = this.crossingRanges(range2);
 
-        if (crossRange.equals(null)) {
-            arrayRanges[0] = range1;
+        if (crossRange == null) {
+            arrayRanges[0] = this;
             arrayRanges[1] = null;
-        } else if (range1.isInside(range2.from) && range2.isInside(range1.to)) {
-            arrayRanges[0] = new Range(range1.from, crossRange.from);
+        } else if (this.isInside(range2.from) && range2.isInside(this.to)) {
+            arrayRanges[0] = new Range(this.from, crossRange.from);
             arrayRanges[1] = null;
-        } else if (range1.isInside(range2.to) && range2.isInside(range1.from)) {
-            arrayRanges[0] = new Range(crossRange.to, range1.to);
+        } else if (this.isInside(range2.to) && range2.isInside(this.from)) {
+            arrayRanges[0] = new Range(crossRange.to, this.to);
             arrayRanges[1] = null;
         } else {
-            arrayRanges[0] = new Range(range1.from, crossRange.from);
-            arrayRanges[1] = new Range(crossRange.to, range1.to);
+            arrayRanges[0] = new Range(this.from, crossRange.from);
+            arrayRanges[1] = new Range(crossRange.to, this.to);
         }
         return arrayRanges;
     }
 
-    public Range crossingRanges(Range range1, Range range2) {
-        if (range1.isInside(range2.to) && range1.isInside(range2.from)) {
+    public Range crossingRanges(Range range2) {
+        if (this.isInside(range2.to) && this.isInside(range2.from)) {
             return range2;
-        } else if (range2.isInside(range1.to) && range2.isInside(range1.from)) {
-            return range1;
-        } else if (range1.isInside(range2.from)) {
-            return new Range(range2.from, range1.to);
-        } else if (range2.isInside(range1.from)) {
-            return new Range(range1.from, range2.to);
+        } else if (range2.isInside(this.to) && range2.isInside(this.from)) {
+            return this;
+        } else if (this.isInside(range2.from)) {
+            return new Range(range2.from, this.to);
+        } else if (range2.isInside(this.from)) {
+            return new Range(this.from, range2.to);
         } else {
             return null;
         }
