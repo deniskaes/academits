5a2f25faa76f11d87106076eb52f2a3112d919e5
Denis <deniskaes@gmail.com>
2018-06-01 01:53:42 +0700

    для проверки уже годится

--- Range/src/ru/academits/esaulov/range/Range.java
+++ Range/src/ru/academits/esaulov/range/Range.java
@@ -4,29 +4,13 @@ public class Range {
     private double from;
     private double to;
 
-    public double getFrom() {
-        return from;
-    }
-
-    public double getTo() {
-        return to;
-    }
-
-    public void setFrom(double from) {
-        this.from = from;
-    }
-
-    public void setTo(double to) {
-        this.to = to;
-    }
-
     public Range(double from, double to) {
         this.from = from;
         this.to = to;
     }
 
     public double getRangeLength() {
-        return Math.abs(this.from - this.to);
+        return Math.abs(from - to);
     }
 
     public boolean isInside(double testNumber) {
