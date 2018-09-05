76e5cd940820a938f2855792fcc939b663d80655
Denis <deniskaes@gmail.com>
2018-09-05 16:46:50 +0700

    На проверку.

--- Matrix/src/ru/academits/esaulov/matrix/Matrix.java
+++ Matrix/src/ru/academits/esaulov/matrix/Matrix.java
@@ -2,8 +2,6 @@ package ru.academits.esaulov.matrix;
 
 import ru.academits.esaulov.vector.Vector;
 
-import java.util.Arrays;
-
 public class Matrix {
     private Vector[] matrix;
 
@@ -18,7 +16,20 @@ public class Matrix {
     }
 
     public Matrix(Vector[] vectorsArray) {
-        matrix = Arrays.copyOf(vectorsArray, vectorsArray.length);
+        int numberLinesMatrix = vectorsArray.length;
+        matrix = new Vector[numberLinesMatrix];
+        int maxLengthVector = 0;
+        for (Vector e : vectorsArray) {
+            maxLengthVector = (e.getSize() > maxLengthVector) ? e.getSize() : maxLengthVector;
+        }
+        for (int i = 0; i < numberLinesMatrix; i++) {
+            int numberCoordinatesVector = vectorsArray[i].getSize();
+            double[] coordinates = new double[numberCoordinatesVector];
+            for (int m = 0; m < numberCoordinatesVector; m++) {
+                coordinates[m] = vectorsArray[i].getCoordinateByIndex(m);
+            }
+            matrix[i] = new Vector(maxLengthVector, coordinates);
+        }
     }
 
     public Matrix(double[][] arrayOfArrays) {
@@ -28,7 +39,9 @@ public class Matrix {
     }
 
     public Matrix(Matrix matrix) {
-        this.matrix = matrix.matrix;
+        for (int i = 0; i < matrix.matrix.length; i++) {
+            this.matrix[i] = new Vector(matrix.matrix[i]);
+        }
     }
 
     public int[] getDimension() {
@@ -49,8 +62,62 @@ public class Matrix {
         if (index < 0 || index > matrix.length) {
             throw new IllegalArgumentException("не верный индекс");
         }
-//        matrix[index] = Arrays.copyOf();
 
+        int dimensionStringVector = getDimension()[1];
+        double[] coordinates = new double[dimensionStringVector];
+        for (int i = 0; i < dimensionStringVector; i++) {
+            coordinates[i] = vector.getCoordinateByIndex(i);
+        }
+        matrix[index] = new Vector(coordinates);
+    }
+
+    public Vector getColumnVector(int index) {
+        if (index < 0 || index > this.getDimension()[1]) {
+            throw new IllegalArgumentException("не верный индекс");
+        }
+        double[] coordinates = new double[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            coordinates[i] = matrix[i].getCoordinateByIndex(index);
+        }
+        return new Vector(coordinates);
+    }
+
+    public void setColumnVector(int index, Vector vector) {
+        if (index < 0 || index > this.getDimension()[1]) {
+            throw new IllegalArgumentException("не верный индекс");
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i].setCoordinateByIndex(index, vector.getCoordinateByIndex(i));
+        }
+    }
+
+    public void transposeMatrix() {
+        int[] dimensionOldMatrix = getDimension();
+        Matrix transposeMatrix = new Matrix(dimensionOldMatrix[1], dimensionOldMatrix[0]);
+        for (int i = 0; i < dimensionOldMatrix[1]; i++) {
+            transposeMatrix.setStringVector(i, getColumnVector(i));
+        }
+        matrix = transposeMatrix.matrix;
+    }
+
+    public void multiplicationByScalar(double number) {
+        for (Vector v : matrix) {
+            v.multiplyVectorByNumber(number);
+        }
+    }
+
+    public void multiplicationMatrixByVector(Vector vector) {
+        int numberColumnMatrix = getDimension()[1];
+        if (numberColumnMatrix != vector.getSize()) {
+            throw new IllegalArgumentException("перемножение не допустимо");
+        }
+        Vector result = new Vector(numberColumnMatrix);
+        for (int i = 0; i < numberColumnMatrix; i++) {
+            result.setCoordinateByIndex(i, Vector.getScalarMultiply(getStringVector(i), vector));
+        }
+        Matrix resultMatrix = new Matrix(numberColumnMatrix, 1);
+        resultMatrix.setColumnVector(0, result);
+        matrix = resultMatrix.matrix;
     }
 
     @Override
@@ -58,9 +125,10 @@ public class Matrix {
         StringBuilder sb = new StringBuilder("Матрица: " + matrix.length + "x" + matrix[0].getSize());
         sb.append(System.lineSeparator());
         sb.append("{");
-        for (int i = 0; i < matrix.length; i++) {
-            sb.append(matrix[i]);
+        for (int i = 0; i < matrix.length - 1; i++) {
+            sb.append(matrix[i]).append(",");
         }
+        sb.append(matrix[matrix.length - 1]);
         sb.append("}");
         return sb.toString();
     }
--- Matrix/src/ru/academits/esaulov/matrix/main/Main.java
+++ Matrix/src/ru/academits/esaulov/matrix/main/Main.java
@@ -1,23 +1,25 @@
 package ru.academits.esaulov.matrix.main;
 
 import ru.academits.esaulov.matrix.Matrix;
+import ru.academits.esaulov.vector.Vector;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(1,3);
+        Matrix matrix = new Matrix(2,2);
         System.out.println(matrix);
 
-        int[] array1 = {1,2,3,4};
 
-        int[] array2 = {1,2};
+        Vector vector1 = new Vector(new double[]{1,2,3});
+        Vector vector2 = new Vector(new double[]{4,5,6});
+        Vector[] av = new Vector[2];
+        av[0] = vector1;
+        av[1] = vector2;
 
-        array2 = Arrays.copyOf(array2,10);
-
-        for(int e: array2){
-            System.out.println(e);
-        }
+        Matrix mat = new Matrix(av);
+        System.out.println(mat);
+        System.out.println(mat.getDimension()[1]);
 
 
 
